72491853ba7b21dae389e75ad473ccd6064ba967
Henry Wandover <henrywandover@gmail.com>
2025-08-30 21:35:32 -0400

    hotfix; entity tracks current scene

--- Entity.hx
+++ Entity.hx
@@ -3,8 +3,8 @@ package wildcat;
 class Entity extends h2d.Object {
 	public var uid( default, null ):UInt;
 
-	public function new( x:Float, y:Float ) {
-		super();
+	public function new( scene:wildcat.Scene, x:Float, y:Float ) {
+		super(scene);
 		setPosition( x, y );
 		uid = Common.makeUniqueId();
 	}
